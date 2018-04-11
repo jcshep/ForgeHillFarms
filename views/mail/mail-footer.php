e6bcbd1c611fcd74396e4c773030b697d2fecf70
jcshep <jcshep@gmail.com>
2018-04-11 13:07:45 -0400

    email updates

---
+++ views/mail/mail-footer.php
@@ -0,0 +1,52 @@
+            <!-- Clear Spacer : BEGIN -->
+            <tr>
+                <td aria-hidden="true" height="40" style="font-size: 0; line-height: 0;">
+                    &nbsp;
+                </td>
+            </tr>
+            <!-- Clear Spacer : END -->
+
+           
+
+        </table>
+        <!-- Email Body : END -->
+
+        
+
+        <!-- Full Bleed Background Section : BEGIN -->
+        <table role="presentation" bgcolor="#174375" cellspacing="0" cellpadding="0" border="0" align="center" width="100%">
+            <tr>
+                <td valign="top" align="center">
+                    <div style="max-width: 600px; margin: auto;" class="email-container">
+                        <!--[if mso]>
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" align="center">
+                        <tr>
+                        <td>
+                        <![endif]-->
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td style="padding: 40px; text-align: center;  font-size: 15px; line-height: 140%; color: #ffffff;">
+                                    <p style="margin: 0;">FORGE HILL FARMS  |  404 CREEK ROAD  |  DOWNINGTOWN, PA 19335</p>
+                                    <img src="https://forgehillfarms.com/images/email-footer-icon.png" alt="Anvil">
+                                </td>
+                            </tr>
+                        </table>
+                        <!--[if mso]>
+                        </td>
+                        </tr>
+                        </table>
+                        <![endif]-->
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <!-- Full Bleed Background Section : END -->
+
+    <!--[if mso | IE]>
+    </td>
+    </tr>
+    </table>
+    <![endif]-->
+    </center>
+</body>
+</html>
--- views/mail/passwordReset.php
+++ views/mail/passwordReset.php
@@ -8,11 +8,27 @@ use yii\helpers\Url;
 
 ?>
 
-<?php //echo $this->render('_header', ['title'=>NULL]); ?>
 
 
-<h2><?= Yii::$app->params['siteName'] ?></h2>
 
-<p>Please click the link below to reset your password and login.</p>
 
-<a class="btn btn-warning" href="<?= Yii::$app->params['siteUrl'] ?>/user/reset-password?auth_key=<?= $auth_key ?>">Reset Password</a>
+
+
+<?= $this->render('mail-header'); ?>
+
+
+<!-- 1 Column Text + Button : BEGIN -->
+<tr>
+	<td bgcolor="#fef8ec" style="padding: 40px 40px 20px; text-align: center;">
+
+		<p>Please click the link below to reset your password and login.</p>
+		
+		<a href="<?= Yii::$app->params['siteUrl'] ?>/user/reset-password?auth_key=<?= $auth_key ?>" style="background: #B93A26; border: 15px solid #B93A26;  font-size: 13px; line-height: 110%; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+			&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#fef8ec;">RESET PASSWORD</span>&nbsp;&nbsp;&nbsp;&nbsp;
+		</a>
+
+	</td>
+</tr>
+
+
+<?= $this->render('mail-footer'); ?>
