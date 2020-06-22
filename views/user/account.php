fca59cdad8b6191a6413a225f75d278e98f16c78
John Sheppard <jcshep@gmail.com>
2020-06-22 14:05:14 -0400

    styling

--- views/user/account.php
+++ views/user/account.php
@@ -46,7 +46,9 @@ $membership_type = Yii::$app->user->identity->membership_type;
 
 				<h3 class="text-center">This Week's Haul</h3>		
 				
+				<div class="text-center">
 				<div class="date"><?php echo date('m.d.Y') ?></div>
+				</div>
 				<?php echo ProductWeek::generateWeeklyList(); ?>
 
 
